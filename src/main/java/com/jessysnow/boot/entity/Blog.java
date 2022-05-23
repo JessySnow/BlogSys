833af4558047ce8a025f9a27cfe49d53255f397f
Jessy Snow <jessysnow12345@gmail.com>
2022-05-23 11:07:41 +0800

    MODIFY -- 修改博客实体类，新增字段 count

--- src/main/java/com/jessysnow/boot/entity/Blog.java
+++ src/main/java/com/jessysnow/boot/entity/Blog.java
@@ -6,6 +6,15 @@ import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.util.Date;
 
+/**
+ * id - 博客 id 自动生成
+ * title - 博客标题
+ * content - 博客正文
+ * pubDate - 博客发布日期
+ * categoryId - 博客类别标签
+ * userId - 写博客的用户 id
+ * count - 博客的阅读次数
+ */
 @Data
 public class Blog implements Serializable {
     private long id;
@@ -14,4 +23,5 @@ public class Blog implements Serializable {
     private Date pubDate;
     private int categoryId;
     private long userId;
+    private long count;
 }
