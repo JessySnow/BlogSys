2898e759e9fcb5b51e74b746316914bc0627bca7
Jessy Snow <jessysnow12345@gmail.com>
2022-05-25 14:00:38 +0800

    MODIFY && ADD -- 删除 Blog 实体类中无用的引用，将建表文件添加到工程目录中

---
+++ sql/createTables.sql
@@ -0,0 +1,72 @@
+/*
+* 数据库建表文件 -- 未进行索引优化
+* 列名规则：单个单词 -- 全小写
+* 	    多个单词 -- 第一个单词首字母小写，
+*			其他单词的首字母大写
+*/
+
+CREATE DATABASE blogsys;
+USE blogsys;
+
+/* 用户表 */
+CREATE TABLE `user`(
+                       id BIGINT PRIMARY KEY AUTO_INCREMENT,
+                       username VARCHAR(50) UNIQUE,
+                       `password` VARCHAR(50),
+                       is_admin BOOLEAN DEFAULT FALSE,
+                       avatar VARCHAR(100) DEFAULT '/photos/violet_evergarden.jpg',
+                       `desc` VARCHAR(200) DEFAULT NULL
+);
+
+/* 类别表 */
+CREATE TABLE category(
+                         id INT PRIMARY KEY AUTO_INCREMENT,
+                         value VARCHAR(30)
+);
+
+/* 博客表 */
+CREATE TABLE blog(
+                     id BIGINT PRIMARY KEY AUTO_INCREMENT,
+                     title VARCHAR(50),
+                     content TEXT,
+                     pub_date DATETIME,
+                     category_id INT,
+                     user_id BIGINT,
+                     `count` BIGINT DEFAULT 0,
+                     FOREIGN KEY(category_id) REFERENCES category(id),
+                     FOREIGN KEY(user_id) REFERENCES `user`(id)
+);
+
+/* 评论表 */
+CREATE TABLE `comment`(
+                          id BIGINT PRIMARY KEY AUTO_INCREMENT,
+                          blog_id BIGINT,
+                          user_id BIGINT,
+                          content VARCHAR(200),
+                          pub_date DATETIME,
+                          FOREIGN KEY(blog_id) REFERENCES blog(id),
+                          FOREIGN KEY(user_id) REFERENCES `user`(id)
+);
+
+/* 点赞表 */
+CREATE TABLE `like`(
+                       user_id BIGINT,
+                       blog_id BIGINT,
+                       PRIMARY KEY(user_id, blog_id),
+                       FOREIGN KEY(user_id) REFERENCES user(id),
+                       FOREIGN KEY(blog_id) REFERENCES blog(id)
+);
+
+/* 点踩表 */
+CREATE TABLE `unlike`(
+                         user_id BIGINT,
+                         blog_id BIGINT,
+                         PRIMARY KEY(user_id, blog_id),
+                         FOREIGN KEY(user_id) REFERENCES user(id),
+                         FOREIGN KEY(blog_id) REFERENCES blog(id)
+);
+
+/*
+ * 索引 : 1. mysql 提供的主键索引
+ *        2. `user` 表的 username 列创建的 unique 索引
+ */
--- src/main/java/com/jessysnow/boot/entity/Blog.java
+++ src/main/java/com/jessysnow/boot/entity/Blog.java
@@ -2,7 +2,6 @@ package com.jessysnow.boot.entity;
 
 import lombok.Data;
 
-import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.util.Date;
 
